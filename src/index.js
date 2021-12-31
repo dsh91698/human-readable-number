60ae59add1a7e7d710e9d0fc6770e69a290bb7c7
dsh91698 <56885229+dsh91698@users.noreply.github.com>
2021-12-31 17:50:10 +0400

    feat: 28 test pass

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const first_ten_numbers = number =>
 const tens10_numbers = number =>
   [`ten`, `eleven`, `twelve`, `thirteen`, `fourteen`, `fifteen`, `sixteen`, `seventeen`, `eighteen`, `nineteen`][number - 10];
 
-const tens_numbers = number =>
-  ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'][number - 10];
+const twentys_numbers = number =>
+  ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'][(number/10 - 2)];
  
 
 
@@ -15,8 +15,11 @@ case 1:
  return first_ten_numbers(number);
 break;
   case 2:
-    if (number) {
+    if (number<20) {
       return tens10_numbers(number);
+    } else if (number >= 20 && parseInt(number.toString()[1])===0) {
+      console.log(number, twentys_numbers(number))
+      return twentys_numbers(number);
     }
  };
 }
