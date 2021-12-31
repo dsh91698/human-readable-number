3e30e1293bc65042e1e50d4847a6d49fa5726aba
dsh91698 <56885229+dsh91698@users.noreply.github.com>
2021-12-31 17:28:13 +0400

    feat: ones and tens - 20 first tests correct

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const first_ten_numbers = number =>
   [`zero`, `one`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`][number];
+
+const tens10_numbers = number =>
+  [`ten`, `eleven`, `twelve`, `thirteen`, `fourteen`, `fifteen`, `sixteen`, `seventeen`, `eighteen`, `nineteen`][number - 10];
+
 const tens_numbers = number =>
-  [`ten`, `eleven`, `twelve`, `thirteen`, `fourteen`, `fifteen`, `sixteen`, `seventeen`, `eighteen`, `nineteen`][number-10];
+  ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'][number - 10];
+ 
 
 
 module.exports = function toReadable (number) {
@@ -9,7 +14,9 @@ switch (number.toString().length) {
 case 1:
  return first_ten_numbers(number);
 break;
-case 2: 
-return tens_numbers(number);
+  case 2:
+    if (number) {
+      return tens10_numbers(number);
+    }
  };
 }
