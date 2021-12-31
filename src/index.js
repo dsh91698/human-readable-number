953119b0ca9177dc9d712a09a727e4bc1e6b16a7
dsh91698 <56885229+dsh91698@users.noreply.github.com>
2021-12-31 17:00:49 +0400

    init: task human readabla number start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const first_ten_numbers = number =>
+  [`zero`, `one`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`][number];
+const tens_numbers = number =>
+  [`ten`, `eleven`, `twelve`, `thirteen`, `fourteen`, `fifteen`, `sixteen`, `seventeen`, `eighteen`, `nineteen`][number-10];
+
+
 module.exports = function toReadable (number) {
-  
+switch (number.toString().length) {
+case 1:
+ return first_ten_numbers(number);
+break;
+case 2: 
+return tens_numbers(number);
+ };
 }
